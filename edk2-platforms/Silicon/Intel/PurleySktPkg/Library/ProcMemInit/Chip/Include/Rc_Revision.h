97a52424c45a346e47005508f448a524621d010a
Isaac Oram <isaac.w.oram@intel.com>
2021-05-05 16:33:03 -0700

    Add missing file header
    Signed-off-by: Isaac Oram <isaac.w.oram@intel.com>

--- edk2-platforms/Silicon/Intel/PurleySktPkg/Library/ProcMemInit/Chip/Include/Rc_Revision.h
+++ edk2-platforms/Silicon/Intel/PurleySktPkg/Library/ProcMemInit/Chip/Include/Rc_Revision.h
@@ -1,3 +1,9 @@
+/** @file
+  Copyright (c) 2018 - 2019, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
 // Declarations and directives
 // -------------------------------------
 // Reference Code (RC) revision in BCD format:
